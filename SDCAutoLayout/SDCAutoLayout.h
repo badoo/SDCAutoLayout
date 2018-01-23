4169992991077079614c8bdb44a04ee9466d3a80
Damir Davletov <damir.davletov@corp.badoo.com>
2018-01-23 15:36:23 +0000

    Added iOS Framework Target to support carthage

---
+++ SDCAutoLayout/SDCAutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  SDCAutoLayout.h
+//  SDCAutoLayout
+//
+//  Created by Damir Davletov on 23/01/2018.
+//  Copyright © 2018 Scotty Doesn't Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDCAutoLayout.
+FOUNDATION_EXPORT double SDCAutoLayoutVersionNumber;
+
+//! Project version string for SDCAutoLayout.
+FOUNDATION_EXPORT const unsigned char SDCAutoLayoutVersionString[];
+
+#import <SDCAutoLayout/UIView+SDCAutoLayout.h>
+
+
